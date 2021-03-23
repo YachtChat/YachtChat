4ce3278349d5bbef35f804e69744b8dbf65548d3
Marc Brede <marc.brede@tum.de>
2021-03-23 15:47:43 +0100

    Update

---
+++ frontend/src/store/spaceSlice.ts
@@ -0,0 +1,43 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+import {Space} from "./models";
+import {AppThunk} from "./store";
+import axios from "axios";
+import {handleError} from "./statusSlice";
+
+const SPACES_URL:string | undefined = process.env.REACT_APP_SPACES_URL;
+
+interface SpaceState {
+    spaces: Space[]
+}
+
+const initialState: SpaceState = {
+    spaces: []
+}
+
+export const spaceSlice = createSlice({
+    name: "space",
+    initialState,
+    reducers: {
+        setSpaces: (state, action: PayloadAction<Space[]>) => {
+            state.spaces = action.payload
+        },
+    }
+});
+
+export const requestSpaces = ():AppThunk => dispatch =>  {
+    if(!SPACES_URL){
+        dispatch(handleError("No websocket url defined for this environment"));
+        return;
+    }
+    axios.get("https://" + SPACES_URL + "/spaces/").then(response => {
+        dispatch(setSpaces(response.data))
+    }).catch(e => console.log(e.trace))
+
+
+}
+
+export const {
+    setSpaces
+} = spaceSlice.actions;
+
+export default spaceSlice.reducer;
