5f9bfdf6761116d7ac27122db3ccc691b40be0ce
Seba <sebastianmarcus.meier@gmail.com>
2021-03-23 13:31:12 +0100

    unify component styles

--- landingpage/src/components/Videocontainer/Videocontainer.tsx
+++ landingpage/src/components/Videocontainer/Videocontainer.tsx
@@ -1,15 +1,26 @@
 import React, {Component} from 'react';
 import './Videocontainer.scss';
 import theVideo from "./demo.mp4";
+import Card from "@material-ui/core/Card";
+import CardMedia from "@material-ui/core/CardMedia";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
 
 
 
 export class Videocontainer extends Component {
     render() {
         return(
-            <video id={`tutorial`} autoPlay playsInline loop muted height="1000" width="1000">
+          <div className="video-container">
+              <Card className="video-card">
+
+
+            <video  id={`tutorial`} autoPlay playsInline loop muted height="1000" width="1000">
                 <source src={theVideo}/>
             </video>
+              </Card>
+          </div>
         );
     }
 
