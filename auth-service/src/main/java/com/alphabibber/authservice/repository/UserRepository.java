c0560bfff0b535a8e1cd0b7ed36b37fafe8525a6
Danilo Walenta <olli.lessly@arcor.de>
2021-04-02 16:57:06 +0200

    Feature/decouple spaces and authentication (#24)
    * Remove all authentication related code from spaces service
    
    * add core functionality for auth-service:
    * Retrieving id, refresh and access tokens.
    * Storing refresh Token in DB.
    * Passing id and access token to frontend.
    
    * implement spaces api as a resource server in oauth2 protocol. endpoints are only accessible if a (google) valid jwt is provided
    
    * remove security from spaces service
    
    * add actuator endpoints
    
    * add auth-service to pipeline
    
    Co-authored-by: Christopher Schütz <schuetzchristopher@icloud.com>

---
+++ auth-service/src/main/java/com/alphabibber/authservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.alphabibber.authservice.repository;
+
+import com.alphabibber.authservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+}
--- spaces-service/src/main/java/com/alphabibber/spacesservice/model/Space.java
+++ spaces-service/src/main/java/com/alphabibber/spacesservice/model/Space.java
@@ -6,8 +6,6 @@ import org.hibernate.annotations.GenericGenerator;
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
-import java.util.SortedSet;
 
 @Entity
 @Table(name = "spaces")
