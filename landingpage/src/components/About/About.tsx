63b91f3ed27971ba90ba6cbac97a45b6c2dbfc85
Seba <sebastianmarcus.meier@gmail.com>
2021-04-24 15:26:14 +0200

    add keycloak object to frontend

---
+++ landingpage/src/components/About/About.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './About.scss';
+
+const About: React.FC = () => (
+  <div id= {`about`} className="About" >
+    About Component
+  </div>
+);
+
+export default About;
---
+++ landingpage/src/types/files.d.ts
@@ -0,0 +1,9 @@
+declare module '*.mp4' {
+    const src: string;
+    export default src;
+}
+
+declare module '*.m4v' {
+    const src: string;
+    export default src;
+}
