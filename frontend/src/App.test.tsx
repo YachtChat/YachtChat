f98c544ab93ce97da2c827b2bf2b823e467192f3
dannlos <olli.lessly@arcor.de>
2021-04-28 20:37:45 +0200

    changed random files

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -4,7 +4,6 @@ import { Provider } from 'react-redux';
 import { store } from './store/store';
 import App from './App';
 
-
 test('renders learn react link', () => {
   const { getByText } = render(
     <Provider store={store}>
