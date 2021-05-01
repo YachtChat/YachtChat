6ee5e8cec11f005aa667d6d2d8fbf9d0b7ef5e1a
Marc Brede <marc.brede@tum.de>
2021-05-02 00:27:37 +0200

    Add Keycloak Admin API Wrapper class as a Service

---
+++ spaces-service/src/main/java/com/alphabibber/spacesservice/service/KeycloakService.java
@@ -0,0 +1,95 @@
+package com.alphabibber.spacesservice.service;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Calendar;
+
+// See here for a documentation of the API
+// https://www.keycloak.org/docs-api/5.0/rest-api/index.html#_users_resource
+
+@Service
+public class KeycloakService {
+    private String accessToken = null;
+    private String refreshToken = null;
+    private Timestamp expiryDateAccessToken = null;
+    private Timestamp expiryDateRefreshToken = null;
+    private String URL = "https://keycloak.alphabibber.com/auth/";
+    private String REALM = "Application";
+    private String PASSWORD = System.getenv("KEYCLOAK_PASSWORD");
+    private String NAME = "Admin";
+
+    public KeycloakService(){
+    }
+
+    public JSONObject getClientById(String id) {
+        checkTokens();
+        HttpResponse<JsonNode> response = Unirest
+                .get("https://keycloak.alphabibber.com/auth/admin/realms/" + REALM + "/users/" + id)
+                .header("Authorization", "Bearer " + accessToken)
+                .asJson();
+        return response.getBody().getObject();
+    }
+
+
+    private JSONObject getClientByEmail(String mail) {
+        checkTokens();
+        HttpResponse<JsonNode> response = Unirest
+                .get("https://keycloak.alphabibber.com/auth/admin/realms/" + REALM + "/users?email=" + mail)
+                .header("Authorization", "Bearer " + accessToken)
+                .asJson();
+        return response.getBody().getObject();
+    }
+
+    private void getNewTokens(){
+        HttpResponse<JsonNode> response = Unirest.post(URL + "realms/master/protocol/openid-connect/token")
+                .header("content-type", "application/x-www-form-urlencoded")
+                .field("username", NAME)
+                .field("password", PASSWORD)
+                .field("client_id", "admin-cli")
+                .field("grant_type", "password")
+                .asJson();
+
+        JSONObject res = response.getBody().getObject();
+        accessToken = res.getString("access_token");
+        refreshToken = res.getString("refresh_token");
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.SECOND, 60);
+        expiryDateAccessToken = new Timestamp(now.getTimeInMillis());
+        now = Calendar.getInstance();
+        now.add(Calendar.SECOND, 1800);
+        expiryDateRefreshToken = new Timestamp(now.getTimeInMillis());
+    }
+
+    private void getNewAccessToken(){
+        HttpResponse<JsonNode> response = Unirest.post(URL + "realms/master/protocol/openid-connect/token")
+                .header("content-type", "application/x-www-form-urlencoded")
+                .field("client_id", "admin-cli")
+                .field("grant_type", "refresh_token")
+                .field("refresh_token", refreshToken)
+                .asJson();
+
+        JSONObject res = response.getBody().getObject();
+        accessToken = res.getString("access_token");
+        Calendar now = Calendar.getInstance();
+        now.add(Calendar.SECOND, 59);
+        expiryDateAccessToken = new Timestamp(now.getTimeInMillis());
+    }
+
+    private void checkTokens(){
+        Calendar now = Calendar.getInstance();
+        if (accessToken == null || expiryDateAccessToken.before(new Timestamp(now.getTimeInMillis()))){
+            if (refreshToken == null || expiryDateRefreshToken.before(new Timestamp(now.getTimeInMillis()))){
+                getNewTokens();
+            }
+            else{
+                getNewAccessToken();
+            }
+        }
+    }
+}
