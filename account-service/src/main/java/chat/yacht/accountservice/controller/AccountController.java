2034898c98792824be685c7ad905d7a0004f8ada
Marc Brede <marc.brede@tum.de>
2021-05-30 16:46:18 +0200

    Add endpoint that allows to fetch multiple users.

--- account-service/src/main/java/chat/yacht/accountservice/controller/AccountController.java
+++ account-service/src/main/java/chat/yacht/accountservice/controller/AccountController.java
@@ -4,6 +4,7 @@ import chat.yacht.accountservice.service.GcpService;
 import chat.yacht.accountservice.service.KeycloakService;
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
+import kong.unirest.json.JSONObject;
 import org.apache.commons.io.FilenameUtils;
 import org.keycloak.KeycloakPrincipal;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
@@ -12,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.security.Principal;
+import java.util.ArrayList;
 import java.util.UUID;
 
 @CrossOrigin(origins = "*")
@@ -31,20 +34,28 @@ public class AccountController extends SpringBootServletInitializer{
     @GetMapping("/email")
     public ResponseEntity<String> getUserByEmail(@RequestParam String email) {
         HttpResponse<JsonNode> response = keycloakService.getUserByEmail(email);
-//      User not found
-//        This does not really work here since keycloak just returns an empty arry when a user with that mail could not
-//        be found
-        return new ResponseEntity<String>(response.getBody().toString(), HttpStatus.valueOf(response.getStatus()));
+        return new ResponseEntity<>(response.getBody().toString(), HttpStatus.valueOf(response.getStatus()));
     }
 
     @GetMapping("/{userId}/")
     public ResponseEntity<String> getUserById(@PathVariable String userId) {
         HttpResponse<JsonNode> response = keycloakService.getUserById(userId);
-//      User not found
         if (response.getStatus() == 404){
-            return new ResponseEntity<String>("A User with id " + userId + " does not exist", HttpStatus.valueOf(404));
+            return new ResponseEntity<>("A User with id " + userId + " does not exist", HttpStatus.valueOf(404));
         }
-        return new ResponseEntity<String>(response.getBody().toString(), HttpStatus.valueOf(response.getStatus()));
+        return new ResponseEntity<>(response.getBody().toString(), HttpStatus.valueOf(response.getStatus()));
+    }
+
+    @GetMapping("/userslist")
+    public ResponseEntity<String> getUserListByIds(@RequestBody ArrayList<String> ids){
+        ArrayList<String> answer = new ArrayList<>();
+        ids.forEach(id -> {
+            HttpResponse<JsonNode> currentResponse = keycloakService.getUserById(id);
+            if (currentResponse.getStatus() != 404){
+                answer.add(currentResponse.getBody().toString());
+            }
+        });
+        return new ResponseEntity<>(String.valueOf(answer), HttpStatus.ACCEPTED);
     }
 
     @PostMapping("/image")
@@ -53,20 +64,20 @@ public class AccountController extends SpringBootServletInitializer{
         // upload file to gcp
         String mimeType = file.getContentType();
         if(! mimeType.startsWith("image")){
-            new ResponseEntity<String>("The File tha was provided was not an image", HttpStatus.BAD_REQUEST);
+            new ResponseEntity<>("The File tha was provided was not an image", HttpStatus.BAD_REQUEST);
         }
 //      TODO: resize image to smaller resolution
         String extension = FilenameUtils.getExtension(file.getOriginalFilename());
         String uniqueID = UUID.randomUUID() + "." + extension;
         String image_url = gcpService.uploadImageToBucket(uniqueID, file);
         HttpResponse<JsonNode> response = keycloakService.updateUserImageById(image_url, token.getSubject());
-        return new ResponseEntity<String>("", HttpStatus.valueOf(response.getStatus()));
+        return new ResponseEntity<>("", HttpStatus.valueOf(response.getStatus()));
     }
 
     @PutMapping("/")
     public ResponseEntity<String> updateUser(@RequestBody String data, Principal principal){
         AccessToken token = ((KeycloakPrincipal) ((KeycloakAuthenticationToken) principal).getPrincipal()).getKeycloakSecurityContext().getToken();
         HttpResponse<JsonNode> ans = keycloakService.updateUserById(data, token.getSubject());
-        return new ResponseEntity<String>("", HttpStatus.valueOf(ans.getStatus()));
+        return new ResponseEntity<>("", HttpStatus.valueOf(ans.getStatus()));
     }
 }
