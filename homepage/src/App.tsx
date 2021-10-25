0964edb5dcc2b955687ce347c1b551e228d5ad7e
tristanratz <triratz@aol.com>
2021-10-25 15:55:04 +0200

    Add navigation bar

--- homepage/src/App.tsx
+++ homepage/src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navigation from "./components/navigation";
+import {ReactKeycloakProvider} from '@react-keycloak/web'
+import {auth} from "./util/keycloak";
+
 
 function App() {
-  return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-  );
+    return (
+        <ReactKeycloakProvider authClient={auth} initOptions={{onLoad: "check-sso"}}>
+
+            <div className="App">
+                <Navigation/>
+            </div>
+        </ReactKeycloakProvider>
+    );
 }
 
 export default App;
