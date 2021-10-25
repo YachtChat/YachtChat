bf161583e16eebe05c58a59d9626422c694396dc
tristanratz <triratz@aol.com>
2021-10-25 13:31:43 +0200

    Initial (docker) configuration

---
+++ homepage/src/util/config.ts
@@ -0,0 +1,7 @@
+export const FRONTEND_URL: string | undefined = process.env.REACT_APP_FRONTEND_URL
+export const SPACES_PORT: string | undefined = process.env.REACT_APP_SPACES_PORT;
+export const AUTH_SERVICE: string | undefined = process.env.REACT_APP_AUTH_URL
+export const REALM: string | undefined = process.env.REACT_APP_REALM
+export const CLIENT_ID: string | undefined = process.env.REACT_APP_KEYCLOAK_CLIENT
+
+export const applicationName: string = "Yacht Chat"
