a4ba9646abdce7bfb4396e71946181c8acad885a
tristanratz <triratz@aol.com>
2021-10-27 14:18:34 +0200

    Add usp

---
+++ homepage/src/components/usp/card.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../tutorial/style.scss"
+import Fade from "react-reveal/Fade";
+
+export class Card extends React.Component {
+
+    render(): React.ReactElement {
+        return (
+            <div className="card">
+                <Fade bottom>
+                    {this.props.children}
+                </Fade>
+            </div>);
+    }
+}
--- homepage/src/components/usp/index.tsx
+++ homepage/src/components/usp/index.tsx
@@ -1,50 +1,61 @@
-import {BiMouse, IoVolumeHigh} from "react-icons/all";
+import {IoBrush, IoLockClosed, IoMdClock} from "react-icons/all";
 import React from "react";
 import "./style.scss"
+import {applicationName} from "../../util/config";
+import classNames from "classnames";
+import {Card} from "./card";
+import {IoMdHeart} from "react-icons/io";
 
 export function Usp() {
     return (
         <div id={"usp"}>
             {/*<div className={"backgroundBall"}/>*/}
+            <svg className={"separator"} width="100%" height="100%" viewBox="0 0 1181 178" version="1.1"
+                 style={{fillRule: "evenodd", clipRule: "evenodd", strokeLinejoin: "round", strokeMiterlimit: 2}}>
+                <g transform="matrix(0.268213,0,0,0.268213,546.275,557.46)">
+                    <circle cx="168" cy="2233" r="4256"/>
+                </g>
+            </svg>
             <div className={"contentWrapper"}>
-                <h1>How to</h1>
-                <h2 className={"subtitle"}>How does Yacht.Chat work?</h2>
-                <div className={"list"}>
+                <h1>Why {applicationName}?</h1>
+                <h2 className={"subtitle"}>What makes {applicationName} so special?</h2>
 
-                    <div className={"listItem"}>
-                        <div className={"itemIcon code"}>
-                            N<span className={"animateicon"} id={"cursoricon"}>_</span>
-                        </div>
-                        <div className={"itemText"}>
-                            After logging in, create a space.
-                            You will instantly receive an invitation link to let others join.
-                            After joining you are a member and since you are now a member you can always return.
-                            A space can be associated with an office or room. <i>Your</i> very own.
-                        </div>
-                    </div>
-
-                    <div className={"listItem"}>
-                        <div className={"itemIcon"}>
-                            <div className={"animateicon"} id={"mouseicon"}><BiMouse/></div>
-                        </div>
-                        <div className={"itemText"}>
-                            After joining you will see the space in all its glory with all the active members..
-                            Click or drag yourself somewhere you want to go.
-                            Easily mute yourself, get an overview over all members or share the screen with the control
-                            bar on the left.
-                        </div>
-                    </div>
-
-                    <div className={"listItem"}>
-                        <div className={"itemIcon code"}>
-                            <div className={"animateicon"} id={"volumeicon"}><IoVolumeHigh/></div>
-                        </div>
-                        <div className={"itemText"}>
-                            Other members can only hear you when they are in your range - the blue halo around you.
-                            You can quickly change the size of your range on the control bar.
-                        </div>
-                    </div>
-
+                <div className={classNames({"aboutCards": true})}>
+                    <Card>
+                        <IoMdHeart/>
+                        <label>Revives teamspirit</label>
+                        <p>
+                            With its unique way of communication, {applicationName} promotes spontaneous conversations
+                            and questions.
+                            The teamspirit will be on new levels - even when working remote.
+                        </p>
+                    </Card>
+                    <Card>
+                        <IoMdClock/>
+                        <label>Save time</label>
+                        <p>
+                            With {applicationName} you can simply talk to anybody in a room.
+                            Don't waste time making appointments and writing messages with your teammates and just talk
+                            to them.
+                        </p>
+                    </Card>
+                    <Card>
+                        <IoBrush/>
+                        <label>Easy to use and understand</label>
+                        <p>
+                            Don't spend time why you do not hear your coworkers and talk to them as you walk to them
+                            with an beautiful and easy UI.
+                        </p>
+                    </Card>
+                    <Card>
+                        <IoLockClosed/>
+                        <label>Privacy at its core</label>
+                        <p>
+                            {applicationName} is a solution build in germany with privacy in mind from the beginning.
+                            Every media connection is based on the peer-to-peer principle, we store as little as
+                            possible data on our servers.
+                        </p>
+                    </Card>
                 </div>
 
             </div>
