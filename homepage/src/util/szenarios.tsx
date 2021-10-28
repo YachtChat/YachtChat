4765b4b79bf1554d62268b5dfc8547c37cf68e00
tristanratz <triratz@aol.com>
2021-10-28 17:21:44 +0200

    Bugfixes

--- homepage/src/util/szenarios.tsx
+++ homepage/src/util/szenarios.tsx
@@ -1,6 +1,8 @@
 import {IoPeopleOutline} from "react-icons/all";
 import {ReactElement} from "react";
 import collaboration from "../rsc/collaboration.jpg"
+import virtualhq from "../rsc/virtualhq.jpg"
+import newemployee from "../rsc/newemployee.jpg"
 
 export interface ScenarioData {
     name: string
@@ -13,7 +15,7 @@ export interface ScenarioData {
 export const getSzenarios = (): ScenarioData[] => {
     return [{
         name: "Virtual HQ",
-        image: collaboration,
+        image: virtualhq,
         text: [
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis metus vulputate enim pretium bibendum. Nulla erat neque, aliquet et turpis ac, placerat gravida nulla. Aliquam tempus, nisl vitae sollicitudin finibus, leo mi tincidunt leo, in pharetra leo ex sed tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi imperdiet diam id felis auctor ornare. Vivamus metus augue, blandit in maximus vitae, rhoncus id magna. Aliquam id tempus ipsum. Ut sollicitudin nulla nec eros vehicula, sed tempus enim venenatis."
         ],
@@ -29,7 +31,7 @@ export const getSzenarios = (): ScenarioData[] => {
         },
         {
             name: "New employee",
-            image: collaboration,
+            image: newemployee,
             text: [
                 "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis metus vulputate enim pretium bibendum. Nulla erat neque, aliquet et turpis ac, placerat gravida nulla. Aliquam tempus, nisl vitae sollicitudin finibus, leo mi tincidunt leo, in pharetra leo ex sed tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi imperdiet diam id felis auctor ornare. Vivamus metus augue, blandit in maximus vitae, rhoncus id magna. Aliquam id tempus ipsum. Ut sollicitudin nulla nec eros vehicula, sed tempus enim venenatis."
             ],
