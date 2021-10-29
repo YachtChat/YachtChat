18f6c3c8712bbb39bb6740201d9a1c83cdd5987e
tristanratz <triratz@aol.com>
2021-10-29 16:38:29 +0200

    Add back keycloak

--- homepage/src/util/keycloak.ts
+++ homepage/src/util/keycloak.ts
@@ -1,13 +1,13 @@
 import Keycloak from "keycloak-js";
-import {FRONTEND_URL} from "./config";
+import {AUTH_SERVICE, CLIENT_ID, FRONTEND_URL, REALM} from "./config";
 
-export const auth = Keycloak();
-
-{
-    // url: AUTH_SERVICE?.includes('localhost') ? 'http://' + AUTH_SERVICE + '/auth' : 'https://' + AUTH_SERVICE + '/auth',
-    // realm: REALM ? REALM : '',
-    // clientId: CLIENT_ID ? CLIENT_ID : ''
-}
+export const auth = Keycloak(
+    {
+        url: AUTH_SERVICE?.includes('localhost') ? 'http://' + AUTH_SERVICE + '/auth' : 'https://' + AUTH_SERVICE + '/auth',
+        realm: REALM ? REALM : '',
+        clientId: CLIENT_ID ? CLIENT_ID : ''
+    }
+);
 
 export const login = () => {
     auth.login({
