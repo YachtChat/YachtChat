ae4a32238e4336a608565871a9fa19ef31d08cb8
tristanratz <triratz@aol.com>
2021-10-29 12:33:21 +0200

    Texts for scenarios + images

--- homepage/src/util/szenarios.tsx
+++ homepage/src/util/szenarios.tsx
@@ -3,6 +3,8 @@ import {ReactElement} from "react";
 import collaboration from "../rsc/collaboration.jpg"
 import virtualhq from "../rsc/virtualhq.jpg"
 import newemployee from "../rsc/newemployee.jpg"
+import coffee from "../rsc/coffee.jpg"
+import workshop from "../rsc/workshop.jpg"
 import {applicationName} from "./config";
 
 export interface ScenarioData {
@@ -15,43 +17,59 @@ export interface ScenarioData {
 
 export const getSzenarios = (): ScenarioData[] => {
     return [
-        {
+        { // Tristan
             name: "Coffee break sessions",
-            image: collaboration,
+            image: coffee,
             text: [
-                ""
+                "Sometimes when working remote some of the more informal exchanges gets lost. " +
+                "Meetings get often more official and scheduled." +
+                "Additionally, when using traditional video conferencing tools only one person at a time can talk. " +
+                applicationName + " promotes dynamic exchange - spontaneously bump into anyone in a space and talk to them. " +
+                "Just like in a hallway or coffee kitchen."
             ],
             icon: <IoCafeOutline/>
         },
-        {
+        { //  Seba
             name: "Virtual HQ",
             image: virtualhq,
             text: [
-                "The whole team at home." + applicationName + "creates an office atmosphere and accompanies you and your team during the working day. The whole team at home. Yacht creates an office atmosphere and accompanies you and your team during the working day. Enter" + applicationName + "at the beginning of your working day, have spontaneous conversations, approach each other and finally collaborate like in the office again."
+                "The whole team at home. " +
+                applicationName + " creates an office atmosphere and accompanies you and your team during the working day. " +
+                "The whole team at home. " +
+                "Yacht creates an office atmosphere and accompanies you and your team during the working day. " +
+                "Enter " + applicationName + " at the beginning of your working day, have spontaneous conversations, approach each other and finally collaborate like in the office again."
             ],
             icon: <IoLaptopOutline/>
         },
-        {
+        { // Tristan
             name: "Collaborative Sessions",
             image: collaboration,
             text: [
-                "No matter if work on a project together or just a specific task that needs consultation – " + applicationName + " will provide you with the necessary tools to shape a productive and creative working environment. Speak to each other when you need to work on something simultaneously or split up into subgroups when you need to focus on a task by yourself and find back as soon as you need to."
+                "No matter if work on a project together or just a specific task that needs consultation – " + applicationName + " will provide you with the necessary tools to shape a productive and creative working environment. " +
+                "Speak to each other when you need to work on something simultaneously or split up into subgroups when you need to focus on a task by yourself and find back as soon as you need to."
             ],
             icon: <IoPeopleOutline/>
         },
-        {
+        { // Seba
             name: "New employee",
             image: newemployee,
             text: [
-                "Integrating new employees into the team is a challenge - especially for remote working teams." + applicationName + "helps you with this task by creating a space and atmosphere where spontaneous conversations and discussions are encouraged. This makes getting to know each other easier and more personal."
+                "Integrating new employees into the team is a challenge - especially for remote working teams. " +
+                applicationName + " helps you with this task by creating a space and atmosphere where spontaneous conversations and discussions are encouraged. " +
+                "Short questions can be easily answered without setting up a new call. " +
+                "This makes getting to know each other easier and more personal. " +
+                "With " + applicationName + " new hires will feel right as part of the team."
             ],
             icon: <IoManOutline/>
         },
-        {
+        { // Tristan
             name: "Workshops",
-            image: newemployee,
+            image: workshop,
             text: [
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis metus vulputate enim pretium bibendum. Nulla erat neque, aliquet et turpis ac, placerat gravida nulla. Aliquam tempus, nisl vitae sollicitudin finibus, leo mi tincidunt leo, in pharetra leo ex sed tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi imperdiet diam id felis auctor ornare. Vivamus metus augue, blandit in maximus vitae, rhoncus id magna. Aliquam id tempus ipsum. Ut sollicitudin nulla nec eros vehicula, sed tempus enim venenatis."
+                "Due to the unique capabilty of dynamically adapting your speaking range, different members can model their role in a space. " +
+                "When a person presents something the speaking range can be maximized to reach a big number of people, " +
+                "and minimized on the other hand when having a private question or discussion about the presentation content. " +
+                "Workgroups can be formed in productivity phases and easily merge back together when presenting something."
             ],
             icon: <IoEaselOutline/>
         }
