003eef819da99b0d9530a42c1c9320712ac7b229
TheOnlySeba <sebastianmarcus.meier@gmail.com>
2021-10-29 12:08:25 +0200

    Added New Employee and Virtual HQ scenario description

--- homepage/src/util/szenarios.tsx
+++ homepage/src/util/szenarios.tsx
@@ -27,7 +27,7 @@ export const getSzenarios = (): ScenarioData[] => {
             name: "Virtual HQ",
             image: virtualhq,
             text: [
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis metus vulputate enim pretium bibendum. Nulla erat neque, aliquet et turpis ac, placerat gravida nulla. Aliquam tempus, nisl vitae sollicitudin finibus, leo mi tincidunt leo, in pharetra leo ex sed tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi imperdiet diam id felis auctor ornare. Vivamus metus augue, blandit in maximus vitae, rhoncus id magna. Aliquam id tempus ipsum. Ut sollicitudin nulla nec eros vehicula, sed tempus enim venenatis."
+                "The whole team at home." + applicationName + "creates an office atmosphere and accompanies you and your team during the working day. The whole team at home. Yacht creates an office atmosphere and accompanies you and your team during the working day. Enter" + applicationName + "at the beginning of your working day, have spontaneous conversations, approach each other and finally collaborate like in the office again."
             ],
             icon: <IoLaptopOutline/>
         },
@@ -43,7 +43,7 @@ export const getSzenarios = (): ScenarioData[] => {
             name: "New employee",
             image: newemployee,
             text: [
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin quis metus vulputate enim pretium bibendum. Nulla erat neque, aliquet et turpis ac, placerat gravida nulla. Aliquam tempus, nisl vitae sollicitudin finibus, leo mi tincidunt leo, in pharetra leo ex sed tellus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Morbi imperdiet diam id felis auctor ornare. Vivamus metus augue, blandit in maximus vitae, rhoncus id magna. Aliquam id tempus ipsum. Ut sollicitudin nulla nec eros vehicula, sed tempus enim venenatis."
+                "Integrating new employees into the team is a challenge - especially for remote working teams." + applicationName + "helps you with this task by creating a space and atmosphere where spontaneous conversations and discussions are encouraged. This makes getting to know each other easier and more personal."
             ],
             icon: <IoManOutline/>
         },
