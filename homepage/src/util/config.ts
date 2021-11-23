ec685764e34042fd814f42c165116981c065b701
tristanratz <triratz@aol.com>
2021-11-23 17:08:31 +0100

    Rename to Yacht.Chat

--- homepage/src/util/config.ts
+++ homepage/src/util/config.ts
@@ -4,7 +4,7 @@ export const AUTH_SERVICE: string | undefined = process.env.REACT_APP_AUTH_URL
 export const REALM: string | undefined = process.env.REACT_APP_REALM
 export const CLIENT_ID: string | undefined = process.env.REACT_APP_KEYCLOAK_CLIENT
 
-export const applicationName: string = "Yacht Chat"
+export const applicationName: string = "Yacht.Chat"
 
 export const INSTA = "https://instagram.com/yacht.chat/"
 export const LINKEDIN = "https://www.linkedin.com/company/yacht-chat/"
