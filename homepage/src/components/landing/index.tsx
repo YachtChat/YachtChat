b062a33fdedb0918088914f196d691e42cf943dd
tristanratz <triratz@aol.com>
2021-11-23 17:12:06 +0100

    Restyle landing screen

--- homepage/src/components/landing/index.tsx
+++ homepage/src/components/landing/index.tsx
@@ -1,46 +1,38 @@
 import "./style.scss"
-import Film from "../../rsc/Film.m4v"
-import Yacht from "../../rsc/yacht_font.png"
-import Yacht_Dark from "../../rsc/yacht_font_white.png"
-import {IoArrowForward, IoLogoInstagram, IoLogoLinkedin, IoLogoTiktok} from "react-icons/all";
 import {useKeycloak} from "@react-keycloak/web";
-import {FRONTEND_URL, INSTA, LINKEDIN, TIKTOK} from "../../util/config";
+import YachtScreenshot from "../../rsc/Landingshot.png"
+import {JoinButton} from "../joinButton";
+import {Links} from "../joinButton/links";
+import Yacht from "../../rsc/yacht.mp4"
+import {Parallax} from "react-scroll-parallax";
 
 export function Landing() {
     const {keycloak} = useKeycloak()
     return (
         <div id={"landing"}>
-            <div className={"video-wrapper"}>
-
-                <video autoPlay playsInline muted loop id="video">
-                    <source src={Film} type="video/mp4"/>
-                </video>
-            </div>
-
             <div id={"intro"}>
                 <div className={"contentWrapper"}>
-                    <div className={"background"}/>
-
+                    <Parallax className={"screenshot"} y={[-50, 50]}>
+                        <img className={"demo light"} src={YachtScreenshot} alt={"Yacht Screenshot"}/>
+                        <video className={"demo dark"} autoPlay loop muted>
+                            <source src={Yacht} type={"video/mp4"}/>
+                        </video>
+                    </Parallax>
                     <div className={"content"}>
 
-                        <img className={"light"} alt={"yacht.chat"} src={Yacht}/>
-                        <img className={"dark"} alt={"yacht.chat"} src={Yacht_Dark}/>
-                        <div className={"headings"}>
-                            <h2>Bring your team home.</h2>
-                            <h2 className={"subheading"}>With the future of communication.</h2>
-                        </div>
+                        <div className={"background"}/>
 
-                        <div className={"button"}
-                             onClick={() => keycloak.login({redirectUri: "https://" + FRONTEND_URL})}>Join a new way
-                            of
-                            working <IoArrowForward/></div>
+                        <div className={"introText"}>
+                            <div className={"headings"}>
+                                <h2>Bring your team home.</h2>
+                                <h2 className={"subheading"}>With the future of communication.</h2>
+                            </div>
 
-                        <br/>
+                            <JoinButton/>
 
-                        <div className={"links"}>
-                            <a href={INSTA}><IoLogoInstagram/></a>
-                            <a href={LINKEDIN}><IoLogoLinkedin/></a>
-                            <a href={TIKTOK}><IoLogoTiktok/></a>
+                            <br/>
+
+                            <Links/>
                         </div>
                     </div>
                 </div>
