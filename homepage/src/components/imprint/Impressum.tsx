fb6036996a9850189693e81d2b5266f30b5d4368
dannlos <mail@danilowalenta.com>
2021-12-21 13:48:41 +0100

    add imprint

---
+++ homepage/src/components/imprint/Impressum.tsx
@@ -0,0 +1,25 @@
+export function Impressum() {
+    // https://www.termsofservicegenerator.net/download.php?lang=en&token=62I9LDi0smDCglshk2xgfLSqJG7b3qjH
+    return (
+        <div id={"impressum"}>
+            <h1>Impressum</h1>
+
+            <h2>Address</h2>
+
+            <p>Yacht.Chat GbR</p>
+            <p>Danilo Walenta</p>
+            <p>Robert-Bosch-Straße 7</p>
+            <p>64293 Darmstadt</p>
+            <p>Germany</p>
+
+            <h2>Legal form</h2>
+
+            <p>Gesellschaft bürgerlichen Rechts (GbR)</p>
+
+            <h2>Contact</h2>
+
+            <p><a href={"mailto:contact@yacht.chat"}>contact@yacht.chat</a></p>
+            <p>+49 6151 3929500</p>
+        </div>
+    )
+}
