45787ff75e9331093de64d180fc8392d8da7d13c
tristanratz <triratz@aol.com>
2022-01-13 16:46:48 +0100

    Status slice posthog integration

--- frontend/src/store/statusSlice.ts
+++ frontend/src/store/statusSlice.ts
@@ -1,5 +1,6 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {AppThunk} from './store';
+import posthog from "posthog-js";
 
 interface StatusState {
     error?: string
@@ -34,16 +35,22 @@ export const {
     resetSuccess
 } = statusSlice.actions;
 
-export const handleError = (error: string, e?: any): AppThunk => (dispatch, getState) => {
+export const handleError = (error: string, e?: any): AppThunk => (dispatch) => {
     console.error(error, e)
     dispatch(setError(error))
     setTimeout(() => dispatch(resetError()), 2000)
+    posthog.capture("[ERROR] " + error)
 }
 
-export const handleSuccess = (success: string): AppThunk => (dispatch, getState) => {
+export const handleSuccess = (success: string): AppThunk => (dispatch) => {
     console.log(success)
     dispatch(setSuccess(success))
     setTimeout(() => dispatch(resetSuccess()), 2000)
+}
+
+export const handleInfo = (info: string): AppThunk => () => {
+    console.log(info)
+    posthog.capture("[INFO] " + info)
 }
 
 export default statusSlice.reducer;
