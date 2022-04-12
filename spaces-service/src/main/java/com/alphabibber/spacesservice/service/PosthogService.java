5d52fac1c9fa5d2f7ba211f836e646388ed83099
Marc Brede <marc.brede@tum.de>
2022-04-12 14:24:25 +0200

    Potentially fixing memory leak

--- spaces-service/src/main/java/com/alphabibber/spacesservice/service/PosthogService.java
+++ spaces-service/src/main/java/com/alphabibber/spacesservice/service/PosthogService.java
@@ -2,20 +2,22 @@ package com.alphabibber.spacesservice.service;
 
 
 import com.posthog.java.PostHog;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
 
 @Service
 public class PosthogService {
-    private final String POSTHOG_API_KEY = "phc_8McKDIRFPbkreZyJSh8A4MtoL4dUHaB7eICFmoPFKsC";
-    private final String POSTHOG_HOST = "https://posthog.yacht.chat";
-    private final PostHog posthog = new PostHog.Builder(POSTHOG_API_KEY).host(POSTHOG_HOST).build();
+    private static final String POSTHOG_API_KEY = "phc_8McKDIRFPbkreZyJSh8A4MtoL4dUHaB7eICFmoPFKsC";
+    private static final String POSTHOG_HOST = "https://posthog.yacht.chat";
+    private static final PostHog posthog = new PostHog.Builder(POSTHOG_API_KEY).host(POSTHOG_HOST).build();
 
+    @Autowired
     public  PosthogService(){
     }
 
-    public void sendEvent(String id, String eventName, Map<String, Object> eventProperties){
+    public static void sendEvent(String id, String eventName, Map<String, Object> eventProperties){
         posthog.capture(id, eventName, eventProperties);
     }
 }
